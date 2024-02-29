0b026fd21b7a1575564db38a9bd128d97d9f87e6
Yaroslav1001 <y.b.grytsishyn@student.khai.edu>
2024-02-29 19:18:02 +0200

    revision_LR_3

--- src/LR_3/LR_3.kt
+++ src/LR_3/LR_3.kt
@@ -1,27 +1,29 @@
 fun main() {
-    val randomChoice = listOf("roc", "paper", "scissors")  // створено список варіантів гри
-    var round = true  // лічильник для гри (якщо "false", то гра закінчується)
-    while (round) {  // цикл гри
+    val randomChoice = listOf("rock", "paper", "scissors")  // створено список варіантів гри
+    while (true) {  // нескінченний цикл гри
         val random = randomChoice.indices.random()  // рандомно обираємо елемент списку посилаючись на його індекс (.indices)
         val choice = randomChoice[random].lowercase()  // отримуємо елемент у вигляді строки, та переводить його до нижнього реєстру
-        println("Random choice: $choice\n")
         print(
-            "Choose an option from the provided ones\n" +
-                    "1.Roc\n2.Paper\n3.Scissors\n> "
+            "Choose an option from the provided ones:\n" +
+                    "Rock\nPaper\nScissors\n!exit - to end the game\n> "
         )
         val userChoice = readlnOrNull()?.lowercase()  // зведення введення користувача до нижнього реєстру
+        if (userChoice == "!exit") { // умова для виходу з гри
+            println("Exiting the game...")
+            break
+        }
         if (userChoice !in randomChoice){  // перевірка на правильний ввід елементів гри
             println("Incorrect input\n")
         } else {
+            println("Enemy selection : $choice\n")
             if (userChoice == choice) {  // умова, при якій збігаються елементи гравців
-                print("Draw\n")
+                println("Draw\n")
             } else if (  // варіанти, при яких користувач виграє
-                (userChoice == "roc" && choice == "scissors") ||  //  перевірка одночасно двох умов за допомогою логічного оператора "та(&&)"
-                (userChoice == "paper" && choice == "roc") ||  // перевірка на збіг усіх умов за допомогою логічного оператора "або(||)"
+                (userChoice == "rock" && choice == "scissors") ||  //  перевірка одночасно двох умов за допомогою логічного оператора "та(&&)"
+                (userChoice == "paper" && choice == "rock") ||  // перевірка на збіг усіх умов за допомогою логічного оператора "або(||)"
                 (userChoice == "scissors" && choice == "paper")
             ) {
                 println("You win")
-                round = false // змінна приймає положення, при якому гра завершується
             } else {
                 println("You lose\n")
             }
