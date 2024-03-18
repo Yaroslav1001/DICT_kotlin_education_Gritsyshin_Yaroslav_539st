2c91758017391d2577f36072204268f09acc2ce6
Yaroslav1001 <y.b.grytsishyn@student.khai.edu>
2024-03-18 11:28:58 +0200

    step-2

--- src/practice/coffeemachine/main.kt
+++ src/practice/coffeemachine/main.kt
@@ -12,4 +12,18 @@ fun main(){
         Coffee is ready! Go to work!
         
     """.trimIndent())
+
+    println("Write how many cups of coffee you will need:")
+    print("> ")
+    val cups = readln().toInt() ?:0
+    val water = 0
+    val milk = 0
+    val coffee = 0
+
+    println("""
+        For $cups cups of coffee you will need:
+        ${200 * cups} ml of water
+        ${50 * cups} ml of milk
+        ${15 * cups} g of coffee beans
+        """.trimIndent())
 }
