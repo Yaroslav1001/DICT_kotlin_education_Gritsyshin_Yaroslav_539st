724ad3a68f966e7445d28036f4554cc6b7212153
Yaroslav1001 <y.b.grytsishyn@student.khai.edu>
2024-03-18 11:28:45 +0200

    step-1

---
+++ src/practice/coffeemachine/main.kt
@@ -0,0 +1,15 @@
+package practice.coffeemachine
+
+fun main(){
+    print("""
+        
+        Starting to make a coffee in NIX office
+        Grinding coffee beans
+        Boiling water
+        Mixing boiled water with crushed coffee beans
+        Pouring coffee into the cup
+        Pouring some milk into the cup
+        Coffee is ready! Go to work!
+        
+    """.trimIndent())
+}
