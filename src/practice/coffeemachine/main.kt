3f04c9f2cf86dd5d84a26d1b48cdaca008634937
Yaroslav1001 <y.b.grytsishyn@student.khai.edu>
2024-03-19 13:35:05 +0200

    step-4

--- src/practice/coffeemachine/main.kt
+++ src/practice/coffeemachine/main.kt
@@ -1,34 +1,108 @@
 package practice.coffeemachine
 
-import kotlin.math.min
+class Coffemachine(
+    private var water: Int,
+    private var milk: Int,
+    private var coffee: Int,
+    private var cups: Int,
+    private var money: Int
+){
+    fun printStatus(){
+        println("""
+            
+            The coffee machine has:
+            $water of water
+            $milk of milk
+            $coffee of coffee beans
+            $cups of disposable cups
+            $money of money
+            
+        """.trimIndent())
+    }
+
+    fun buy(type: String){
+        when (type) {
+            "1", "espresso" -> {
+                if (water >= 250 && coffee >= 16 && cups >= 1){
+                    money += 4
+                    water -= 250
+                    coffee -= 16
+                    cups -= 1
+                }
+            }
+            "2", "latte" -> {
+                if (water >= 350 && milk >= 75 && coffee >= 20 && cups >= 1) {
+                    money += 7
+                    water -= 350
+                    milk -= 75
+                    coffee -= 20
+                    cups -= 1
+                }
+            }
+            "3", "cappuccino" -> {
+                if (water >= 200 && milk >= 100 &&  coffee >= 12 && cups >= 1){
+                    money += 6
+                    water -= 200
+                    milk -= 100
+                    coffee -= 12
+                    cups -= 1
+                }
+            }
+            else -> println("Invalid choice!")
+        }
+        printStatus()
+    }
+
+    fun fill(addWater: Int, addMilk: Int, addCoffee: Int, addCups: Int){
+        water += addWater
+        milk += addMilk
+        coffee += addCoffee
+        cups += addCups
+        printStatus()
+    }
+
+    fun take(){
+        println("I gave you $money")
+        money = 0
+        printStatus()
+    }
+
+}
 
 fun main(){
 
-    println("Write how many ml of water the coffee machine has:")
-    print("> ")
-    val water = readln().toInt()
-
-    println("Write how many ml of milk the coffee machine has:")
-    print("> ")
-    val milk = readln().toInt()
-
-    println("Write how many grams of coffee beans the coffee machine has:")
-    print("> ")
-    val coffee = readln().toInt()
-
-    println("Write how many cups of coffee you will need:")
-    print("> ")
-    val cups = readln().toInt()
-
-    val cupsPossible = minOf(water / 200, milk / 50, coffee / 15)
-
-    if (cupsPossible >= cups) {
-        if (cupsPossible == cups) {
-            println("Yes, I can make that amount of coffee")
-        } else {
-            print("Yes, I can make that amount of coffee (and even ${cupsPossible - cups} more than that)")
+    val coffeeMachine = Coffemachine(400, 540, 120, 9, 550)
+    coffeeMachine.printStatus()
+    while (true){
+        println("Write action (buy, fill, take):")
+        print("> ")
+        when(readln().trim()){
+            "buy" -> {
+                println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+                print("> ")
+                coffeeMachine.buy(readln().trim())
+            }
+            "fill" -> {
+                println("\nWrite how many ml of water do you want to add:")
+                print("> ")
+                val water = readln().toInt()
+                println("Write how many ml of milk do you want to add:")
+                print("> ")
+                val milk = readln().toInt()
+                println("Write how many grams of coffee beans do you want to add:")
+                print("> ")
+                val coffee = readln().toInt()
+                println("Write how many disposable cups of coffee do you want to add:")
+                print("> ")
+                val cups = readln().toInt()
+                coffeeMachine.fill(water, milk, coffee, cups)
+            }
+            "take" -> {
+                coffeeMachine.take()
+            }
+            else -> println("Invalid choice!")
         }
-    } else {
-        println("No, I can make only $cupsPossible cups of coffee")
     }
+
+
 }
