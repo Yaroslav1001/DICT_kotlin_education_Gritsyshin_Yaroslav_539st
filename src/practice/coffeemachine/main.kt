c69184690d743153a906e4440a04c739aa162990
Yaroslav1001 <y.b.grytsishyn@student.khai.edu>
2024-03-19 14:07:40 +0200

    step-5

--- src/practice/coffeemachine/main.kt
+++ src/practice/coffeemachine/main.kt
@@ -1,13 +1,13 @@
 package practice.coffeemachine
 
-class Coffemachine(
+class CoffeeMachine(
     private var water: Int,
     private var milk: Int,
     private var coffee: Int,
     private var cups: Int,
     private var money: Int
 ){
-    fun printStatus(){
+    fun remaining(){
         println("""
             
             The coffee machine has:
@@ -21,36 +21,56 @@ class Coffemachine(
     }
 
     fun buy(type: String){
+        var notEnought = ""
+        if (cups < 1) notEnought += "disposable cups"
         when (type) {
             "1", "espresso" -> {
+                if (water < 250) notEnought += "water"
+                if (coffee < 16) notEnought += "coffee beans"
                 if (water >= 250 && coffee >= 16 && cups >= 1){
+                    println("I have enough resources, making you a coffee!")
                     money += 4
                     water -= 250
                     coffee -= 16
                     cups -= 1
+                } else{
+                    println("Sorry, not enough ${notEnought}!")
                 }
             }
             "2", "latte" -> {
+                if (water < 350) notEnought += "water"
+                if (milk < 75) notEnought += "milk"
+                if (coffee < 20) notEnought += "coffee beans"
                 if (water >= 350 && milk >= 75 && coffee >= 20 && cups >= 1) {
+                    println("I have enough resources, making you a coffee!")
                     money += 7
                     water -= 350
                     milk -= 75
                     coffee -= 20
                     cups -= 1
+                } else{
+                    println("Sorry, not enough ${notEnought}!")
                 }
             }
             "3", "cappuccino" -> {
+                if (water < 200) notEnought += "water"
+                if (milk < 100) notEnought += "milk"
+                if (coffee < 12) notEnought += "coffee beans"
                 if (water >= 200 && milk >= 100 &&  coffee >= 12 && cups >= 1){
+                    println("I have enough resources, making you a coffee!")
                     money += 6
                     water -= 200
                     milk -= 100
                     coffee -= 12
                     cups -= 1
+                } else{
+                    println("Sorry, not enough ${notEnought}!")
                 }
             }
+            "back" -> { return }
+
             else -> println("Invalid choice!")
         }
-        printStatus()
     }
 
     fun fill(addWater: Int, addMilk: Int, addCoffee: Int, addCups: Int){
@@ -58,30 +78,28 @@ class Coffemachine(
         milk += addMilk
         coffee += addCoffee
         cups += addCups
-        printStatus()
     }
 
     fun take(){
         println("I gave you $money")
         money = 0
-        printStatus()
     }
 
 }
 
-fun main(){
+fun main() {
 
-    val coffeeMachine = Coffemachine(400, 540, 120, 9, 550)
-    coffeeMachine.printStatus()
-    while (true){
-        println("Write action (buy, fill, take):")
+    val coffeeMachine = CoffeeMachine(400, 540, 120, 9, 550)
+    while (true) {
+        println("Write action (buy, fill, take,remaining, exit):")
         print("> ")
-        when(readln().trim()){
+        when (readln().trim()) {
             "buy" -> {
-                println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+                println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back – to main menu:")
                 print("> ")
                 coffeeMachine.buy(readln().trim())
             }
+
             "fill" -> {
                 println("\nWrite how many ml of water do you want to add:")
                 print("> ")
@@ -97,12 +115,20 @@ fun main(){
                 val cups = readln().toInt()
                 coffeeMachine.fill(water, milk, coffee, cups)
             }
+
             "take" -> {
                 coffeeMachine.take()
             }
+
+            "remaining" -> {
+                coffeeMachine.remaining()
+            }
+
+            "exit" -> {
+                return
+            }
+
             else -> println("Invalid choice!")
         }
     }
-
-
 }
